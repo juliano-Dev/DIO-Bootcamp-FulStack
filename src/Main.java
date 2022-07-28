952bde3ede0158c983e99097105de8c1e99f8102
Juliano Gomes <julianocg2013@gmail.com>
2022-07-27 23:36:55 -0300

    Primeiro commit projeto Curso DIO

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Main {
+import br.com.dio.desafio.dominio.Curso;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Java OOP");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JS");
+        curso2.setDescricao("JS Completo");
+        curso2.setCargaHoraria(8);
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+    }
 }
